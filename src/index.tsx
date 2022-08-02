eb33c2027003d97c9fcd6b7bef73d809a73f7e6f
huseyinsartas <hsynsrtss@gmail.com>
2022-08-02 16:00:01 +0300

    dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { CustomProvider } from "rsuite";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <CustomProvider theme="dark">
+      <App />
+    </CustomProvider>
   </React.StrictMode>
 );
 
