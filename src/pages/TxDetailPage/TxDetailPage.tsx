46c4968ce7460df97be2454f2989d3f1263a4f95
huseyinsartas <hsynsrtss@gmail.com>
2022-08-03 00:43:17 +0300

    TxDetailPage: useHistory removed

--- src/pages/TxDetailPage/TxDetailPage.tsx
+++ src/pages/TxDetailPage/TxDetailPage.tsx
@@ -1,10 +1,8 @@
-import { useHistory, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { TX_DETAIL } from "../../app/ROUTE";
 
 export const TxDetailPage = () => {
   document.title = TX_DETAIL.TITLE;
-
-  const history = useHistory();
 
   const { id } = useParams<{ id: string }>();
 
