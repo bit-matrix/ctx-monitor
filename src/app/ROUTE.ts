1c8e34a7c752a30b31cc19dfddbce0982a1ca28a
huseyinsartas <hsynsrtss@gmail.com>
2022-08-03 00:42:33 +0300

    TX_DETAIL: title fix

--- src/app/ROUTE.ts
+++ src/app/ROUTE.ts
@@ -10,7 +10,7 @@ export const HOME: RouteType = { PATH: "/", TITLE: "Home" };
 export const TXS: RouteType = { PATH: "/txs", TITLE: "Transactions" };
 
 // detail route
-export const TX_DETAIL: RouteType = { PATH: "/tx", TITLE: "Transaction" };
+export const TX_DETAIL: RouteType = { PATH: "/tx", TITLE: "Transaction Detail" };
 
 // not found
 export const NOT_FOUND: RouteType = { PATH: "*", TITLE: "Page Not Found :(" };
