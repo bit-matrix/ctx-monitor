283097b799fdc466c1ced57ac85723712d64a523
huseyinsartas <hsynsrtss@gmail.com>
2022-08-02 20:21:04 +0300

    Txs Page

--- src/pages/TxsPage/TxsPage.tsx
+++ src/pages/TxsPage/TxsPage.tsx
@@ -1,7 +1,20 @@
 import { TXS } from "../../app/ROUTE";
+import { Tx } from "../../components/Tx/Tx";
+import { data } from "../../mockData/mockdata";
+import "./TxsPage.scss";
 
 export const TxsPage = () => {
   document.title = TXS.TITLE;
 
-  return <div>Txs</div>;
+  return (
+    <div className="transactionsTable">
+      <h3 className="transactionsTableTitle">Commitment Transactions</h3>
+      <div className="transactionsTableRow header">
+        <div className="transactionsTableCell">Transaction ID</div>
+      </div>
+      {data.map((d) => (
+        <Tx key={d.commitmentData.transaction.txid} data={d} />
+      ))}
+    </div>
+  );
 };
