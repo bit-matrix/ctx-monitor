d8039217d989c5efefb3684207c7cdd1abc2c9a7
huseyinsartas <hsynsrtss@gmail.com>
2022-08-02 20:23:12 +0300

    TxDetailPage page start

--- src/pages/TxDetailPage/TxDetailPage.tsx
+++ src/pages/TxDetailPage/TxDetailPage.tsx
@@ -1,7 +1,16 @@
+import { useHistory, useParams } from "react-router-dom";
 import { TX_DETAIL } from "../../app/ROUTE";
 
 export const TxDetailPage = () => {
   document.title = TX_DETAIL.TITLE;
 
-  return <div>TxDetail</div>;
+  const history = useHistory();
+
+  const { id } = useParams<{ id: string }>();
+
+  return (
+    <div>
+      <div>{id}</div>
+    </div>
+  );
 };
