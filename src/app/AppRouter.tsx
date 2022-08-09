8a4f86185c945c61090aba54a35769c8686214e3
huseyinsartas <hsynsrtss@gmail.com>
2022-08-09 18:11:53 +0300

    Search

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Loading } from "../components/Loading/Loading";
 import { PageLayout } from "../components/PageLayout/PageLayout";
@@ -10,11 +11,25 @@ import "./AppRouter.scss";
 import * as ROUTE from "./ROUTE";
 
 const AppRouter = () => {
+  const [search, setSearch] = useState<string>();
+  const [filteredData, setFilteredData] = useState<any>();
+
   const { data, isConnected, loading } = useSocket();
+
+  useEffect(() => {
+    let txs = [];
+
+    if (!search || search === "") {
+      txs = data;
+    } else {
+      txs = data.filter((dt: any) => dt.commitmentData.transaction.txid === search);
+    }
+    setFilteredData(txs);
+  }, [data, search]);
 
   return (
     <BrowserRouter>
-      <PageLayout>
+      <PageLayout searchText={(text: string) => setSearch(text)}>
         {loading ? (
           <div className="appLoading">
             <Loading width="2.5rem" height="2.5rem" />
@@ -23,8 +38,8 @@ const AppRouter = () => {
           <div className="appContent">
             {isConnected ? (
               <Routes>
-                <Route path={ROUTE.HOME.PATH} element={<TxsPage txs={data} />} />
-                <Route path={ROUTE.TXS.PATH} element={<TxsPage txs={data} />} />
+                <Route path={ROUTE.HOME.PATH} element={<TxsPage txs={filteredData} />} />
+                {/* <Route path={ROUTE.TXS.PATH} element={<TxsPage txs={filteredData} />} /> */}
                 <Route path={`${ROUTE.TX_DETAIL.PATH}/:id`} element={<TxDetailPage txs={data} />} />
                 <Route path={ROUTE.NOT_FOUND.PATH} element={<NotFoundPage />} />
               </Routes>
--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router";
 import { HOME } from "../../app/ROUTE";
 import Home from "../Svg/Icons/Home";
 import "./PageLayout.scss";
 
 type Props = {
   children?: React.ReactNode;
+  searchText: (text: string) => void;
 };
 
-export const PageLayout: React.FC<Props> = ({ children }) => {
+export const PageLayout: React.FC<Props> = ({ children, searchText }) => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <div className="content">
@@ -19,9 +22,21 @@ export const PageLayout: React.FC<Props> = ({ children }) => {
             <Home />
           </button>
         </div>
-        {/* <div className="searchBar">
-          <input className="searchBarInput" type="search" name="q" placeholder="Search" autoFocus required autoComplete="off" aria-label="Search" />
-        </div> */}
+        {location.pathname === HOME.PATH && (
+          <div className="searchBar">
+            <input
+              className="searchBarInput"
+              type="search"
+              name="q"
+              placeholder="Search"
+              autoFocus
+              required
+              autoComplete="off"
+              aria-label="Search"
+              onChange={(event) => searchText(event.target.value)}
+            />
+          </div>
+        )}
       </div>
       <div className="container">{children}</div>
     </div>
